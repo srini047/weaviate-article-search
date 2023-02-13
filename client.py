07939a06741af4ca2b5c880c664d4b4661aad26f
srini047 <srinikethcr7@gmail.com>
2023-02-13 23:28:34 +0530

    fix: env variables

--- client.py
+++ client.py
@@ -2,9 +2,12 @@ import weaviate
 import os
 from dotenv import load_dotenv
 load_dotenv()
+# import streamlit as st
 
 Client = weaviate.Client(
     url=os.environ.get("WEAVIATE_URL"),  # Replace with your endpoint
+    # url=st.secrets["WEAVIATE_URL"] 
     additional_headers={
         "X-Cohere-Api-Key":os.environ.get("COHERE_API_KEY")  # Replace with your API key
+        # "X-Cohere-Api-Key":st.secrets["COHERE_API_KEY"]
     })
